ef98b67b60c841945c1097cef10fb57945d2f49b
むずでょ <muzudho1@gmail.com>
2021-11-06 20:46:10 +0900

    print board

--- scripts/position.py
+++ scripts/position.py
@@ -18,7 +18,7 @@ class Position():
         self._begin_pos_row_pattern = re.compile(
             r"^P(\d)(.{3})(.{3})(.{3})(.{3})(.{3})(.{3})(.{3})(.{3})(.{3})$")
 
-        # 指し手
+        # 自分または相手の指し手と、その消費時間
         # Format: `<先後><元升><先升><駒>,T<秒>`
         # Example: `+5756FU,T20`
         self._move_pattern = re.compile(
@@ -29,6 +29,9 @@ class Position():
 
         # 持駒の数 [未使用, ▲飛, ▲角, ▲金, ▲銀, ▲桂, ▲香, ▲歩, ▽飛, ▽角, ▽金, ▽銀, ▽桂, ▽香, ▽歩]
         self._hands = [0] * 15
+
+        # 経過時間 [未使用, 先手, 後手]
+        self._expendTimes = [0, 0, 0]
 
     @property
     def board(self):
@@ -61,6 +64,8 @@ class Position():
             phase = result.group(1)
             source = int(result.group(2))
             destination = int(result.group(3))
+            expendTime = int(result.group(5))
+
             piece = result.group(4)
             srcPc = self._board[source]  # sourcePiece
             dstPc = self._board[destination]  # destinationPiece
@@ -151,6 +156,12 @@ class Position():
             # 移動先に駒を置く
             self._board[destination] = srcPc
 
+            # 経過時間
+            if phase == '+':
+                self._expendTimes[1] += expendTime
+            else:
+                self._expendTimes[2] += expendTime
+
             return '<Position.Move/>'
 
         return '<Position.Unknown/>'
@@ -158,134 +169,84 @@ class Position():
     def printBoard(self):
         """将棋盤の描画"""
 
-        # 後手の持ち駒
-        a = '{: >3}'.format(self._hands[8])
-        b = '{: >3}'.format(self._hands[9])
-        c = '{: >3}'.format(self._hands[10])
-        d = '{: >3}'.format(self._hands[11])
-        e = '{: >3}'.format(self._hands[12])
-        f = '{: >3}'.format(self._hands[13])
-        g = '{: >3}'.format(self._hands[14])
-        print(f" HI  KA  KI  GI  KE  KY  FU  ")
-        print(f"+---+---+---+---+---+---+---+")
-        print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|")
-        print(f"+---+---+---+---+---+---+---+")
+        def __prettyHands(indexes):
+            a = '{: >3}'.format(self._hands[indexes[0]])
+            b = '{: >3}'.format(self._hands[indexes[1]])
+            c = '{: >3}'.format(self._hands[indexes[2]])
+            d = '{: >3}'.format(self._hands[indexes[3]])
+            e = '{: >3}'.format(self._hands[indexes[4]])
+            f = '{: >3}'.format(self._hands[indexes[5]])
+            g = '{: >3}'.format(self._hands[indexes[6]])
+            return a, b, c, d, e, f, g
+
+        def __prettyBoardRow(indexes):
+            a = '{: >3}'.format(self._board[indexes[0]])
+            b = '{: >3}'.format(self._board[indexes[1]])
+            c = '{: >3}'.format(self._board[indexes[2]])
+            d = '{: >3}'.format(self._board[indexes[3]])
+            e = '{: >3}'.format(self._board[indexes[4]])
+            f = '{: >3}'.format(self._board[indexes[5]])
+            g = '{: >3}'.format(self._board[indexes[6]])
+            h = '{: >3}'.format(self._board[indexes[7]])
+            i = '{: >3}'.format(self._board[indexes[8]])
+            return a, b, c, d, e, f, g, h, i
+
+        # 後手の持ち駒、経過時間
+        tim2 = f'{self._expendTimes[2]: >6}'
+        a, b, c, d, e, f, g = __prettyHands([8, 9, 10, 11, 12, 13, 14])
+        print(f" HI  KA  KI  GI  KE  KY  FU   TIME")
+        print(f"+---+---+---+---+---+---+---+ ")
+        print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}| {tim2}")
+        print(f"+---+---+---+---+---+---+---+ ")
         print(f"")
         # 盤
         print(f"  9   8   7   6   5   4   3   2   1    ")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        a = '{: >3}'.format(self._board[91])
-        b = '{: >3}'.format(self._board[81])
-        c = '{: >3}'.format(self._board[71])
-        d = '{: >3}'.format(self._board[61])
-        e = '{: >3}'.format(self._board[51])
-        f = '{: >3}'.format(self._board[41])
-        g = '{: >3}'.format(self._board[31])
-        h = '{: >3}'.format(self._board[21])
-        i = '{: >3}'.format(self._board[11])
+        a, b, c, d, e, f, g, h, i = __prettyBoardRow(
+            [91, 81, 71, 61, 51, 41, 31, 21, 11])
         print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|{h}|{i}| 1")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        a = '{: >3}'.format(self._board[92])
-        b = '{: >3}'.format(self._board[82])
-        c = '{: >3}'.format(self._board[72])
-        d = '{: >3}'.format(self._board[62])
-        e = '{: >3}'.format(self._board[52])
-        f = '{: >3}'.format(self._board[42])
-        g = '{: >3}'.format(self._board[32])
-        h = '{: >3}'.format(self._board[22])
-        i = '{: >3}'.format(self._board[12])
+        a, b, c, d, e, f, g, h, i = __prettyBoardRow(
+            [92, 82, 72, 62, 52, 42, 32, 22, 12])
         print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|{h}|{i}| 2")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        a = '{: >3}'.format(self._board[93])
-        b = '{: >3}'.format(self._board[83])
-        c = '{: >3}'.format(self._board[73])
-        d = '{: >3}'.format(self._board[63])
-        e = '{: >3}'.format(self._board[53])
-        f = '{: >3}'.format(self._board[43])
-        g = '{: >3}'.format(self._board[33])
-        h = '{: >3}'.format(self._board[23])
-        i = '{: >3}'.format(self._board[13])
+        a, b, c, d, e, f, g, h, i = __prettyBoardRow(
+            [93, 83, 73, 63, 53, 43, 33, 23, 13])
         print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|{h}|{i}| 3")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        a = '{: >3}'.format(self._board[94])
-        b = '{: >3}'.format(self._board[84])
-        c = '{: >3}'.format(self._board[74])
-        d = '{: >3}'.format(self._board[64])
-        e = '{: >3}'.format(self._board[54])
-        f = '{: >3}'.format(self._board[44])
-        g = '{: >3}'.format(self._board[34])
-        h = '{: >3}'.format(self._board[24])
-        i = '{: >3}'.format(self._board[14])
+        a, b, c, d, e, f, g, h, i = __prettyBoardRow(
+            [94, 84, 74, 64, 54, 44, 34, 24, 14])
         print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|{h}|{i}| 4")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        a = '{: >3}'.format(self._board[95])
-        b = '{: >3}'.format(self._board[85])
-        c = '{: >3}'.format(self._board[75])
-        d = '{: >3}'.format(self._board[65])
-        e = '{: >3}'.format(self._board[55])
-        f = '{: >3}'.format(self._board[45])
-        g = '{: >3}'.format(self._board[35])
-        h = '{: >3}'.format(self._board[25])
-        i = '{: >3}'.format(self._board[15])
+        a, b, c, d, e, f, g, h, i = __prettyBoardRow(
+            [95, 85, 75, 65, 55, 45, 35, 25, 15])
         print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|{h}|{i}| 5")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        a = '{: >3}'.format(self._board[96])
-        b = '{: >3}'.format(self._board[86])
-        c = '{: >3}'.format(self._board[76])
-        d = '{: >3}'.format(self._board[66])
-        e = '{: >3}'.format(self._board[56])
-        f = '{: >3}'.format(self._board[46])
-        g = '{: >3}'.format(self._board[36])
-        h = '{: >3}'.format(self._board[26])
-        i = '{: >3}'.format(self._board[16])
+        a, b, c, d, e, f, g, h, i = __prettyBoardRow(
+            [96, 86, 76, 66, 56, 46, 36, 26, 16])
         print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|{h}|{i}| 6")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        a = '{: >3}'.format(self._board[97])
-        b = '{: >3}'.format(self._board[87])
-        c = '{: >3}'.format(self._board[77])
-        d = '{: >3}'.format(self._board[67])
-        e = '{: >3}'.format(self._board[57])
-        f = '{: >3}'.format(self._board[47])
-        g = '{: >3}'.format(self._board[37])
-        h = '{: >3}'.format(self._board[27])
-        i = '{: >3}'.format(self._board[17])
+        a, b, c, d, e, f, g, h, i = __prettyBoardRow(
+            [97, 87, 77, 67, 57, 47, 37, 27, 17])
         print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|{h}|{i}| 7")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        a = '{: >3}'.format(self._board[98])
-        b = '{: >3}'.format(self._board[88])
-        c = '{: >3}'.format(self._board[78])
-        d = '{: >3}'.format(self._board[68])
-        e = '{: >3}'.format(self._board[58])
-        f = '{: >3}'.format(self._board[48])
-        g = '{: >3}'.format(self._board[38])
-        h = '{: >3}'.format(self._board[28])
-        i = '{: >3}'.format(self._board[18])
+        a, b, c, d, e, f, g, h, i = __prettyBoardRow(
+            [98, 88, 78, 68, 58, 48, 38, 28, 18])
         print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|{h}|{i}| 8")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        a = '{: >3}'.format(self._board[99])
-        b = '{: >3}'.format(self._board[89])
-        c = '{: >3}'.format(self._board[79])
-        d = '{: >3}'.format(self._board[69])
-        e = '{: >3}'.format(self._board[59])
-        f = '{: >3}'.format(self._board[49])
-        g = '{: >3}'.format(self._board[39])
-        h = '{: >3}'.format(self._board[29])
-        i = '{: >3}'.format(self._board[19])
+        a, b, c, d, e, f, g, h, i = __prettyBoardRow(
+            [99, 89, 79, 69, 59, 49, 39, 29, 19])
         print(f"|{a}|{b}|{c}|{d}|{e}|{f}|{g}|{h}|{i}| 9")
         print(f"+---+---+---+---+---+---+---+---+---+  ")
-        # 先手の持ち駒
+        # 先手の経過時間、持ち駒
+        tim1 = f'{self._expendTimes[1]: >6}'
+        a, b, c, d, e, f, g = __prettyHands([1, 2, 3, 4, 5, 6, 7])
         print(f"")
-        a = '{: >3}'.format(self._hands[1])
-        b = '{: >3}'.format(self._hands[2])
-        c = '{: >3}'.format(self._hands[3])
-        d = '{: >3}'.format(self._hands[4])
-        e = '{: >3}'.format(self._hands[5])
-        f = '{: >3}'.format(self._hands[6])
-        g = '{: >3}'.format(self._hands[7])
-        print(f"         HI  KA  KI  GI  KE  KY  FU  ")
+        print(f" Time    HI  KA  KI  GI  KE  KY  FU  ")
         print(f"        +---+---+---+---+---+---+---+")
-        print(f"        |{a}|{b}|{c}|{d}|{e}|{f}|{g}|")
+        print(f" {tim1} |{a}|{b}|{c}|{d}|{e}|{f}|{g}|")
         print(f"        +---+---+---+---+---+---+---+")
+        print(f"")
 
 
 # Test
